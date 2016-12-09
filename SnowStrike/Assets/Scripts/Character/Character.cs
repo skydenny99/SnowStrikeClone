44dbc6e47a51dec9d8e3d7c8b646d06f847d287d
yangju <yangju0w0@gmail.com>
2016-12-10 02:32:52 +0900

    좌우이동 및 점프, 단 다단점프 가능

--- SnowStrike/Assets/Scripts/Character/Character.cs
+++ SnowStrike/Assets/Scripts/Character/Character.cs
@@ -1,16 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Character : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+﻿public interface Character
+{
+    void Move();
+    void Attack();
 }
---
+++ SnowStrike/Assets/Scripts/Character/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, Character {
+
+    public int HP;
+
+    //Horizontal movement
+    public float acceleration;
+    public float maxSpeed;
+
+    public float toZero;
+    private float _vx;
+    private Rigidbody2D _rig;
+
+    //Vertical movement
+    public float jumpPower;
+    public bool jumpAble = false;
+
+    
+    
+    //Equipment currentEquipment;
+
+
+	// Use this for initialization
+	void Start () {
+        _rig = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move()
+    {
+        float axis = Input.GetAxis("Horizontal");
+        Vector2 v = new Vector2(axis * acceleration, 0);
+        _rig.AddForce(v);
+
+        float temp = Mathf.Clamp(_rig.velocity.x, -maxSpeed, maxSpeed);
+        if(Mathf.Abs(axis) < 0.8)
+            temp = Mathf.SmoothDamp(temp, 0, ref _vx, toZero);
+        _rig.velocity = new Vector2(temp, _rig.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && jumpAble)
+        {
+            _rig.velocity = new Vector2(_rig.velocity.x, jumpPower);
+            //jumpAble = false;
+        }
+    }
+
+    public bool isGrounded()
+    {
+        return false;
+    }
+
+    public void Attack()
+    {
+        
+    }
+
+    public void SwapWeapon()
+    {
+        
+    }
+}
