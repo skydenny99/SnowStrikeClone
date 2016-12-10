d481b78b900924cf066d340ee27029a90cbf25fc
yangju <yangju0w0@gmail.com>
2016-12-10 15:17:55 +0900

    MOD : 샘플 아이템 추가

--- SnowStrike/Assets/Scripts/Item/Equipment.cs
+++ SnowStrike/Assets/Scripts/Item/Equipment.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Equipment : MonoBehaviour, Ownable {
-    protected int level = 1;
-    public EquipmentType equipType = EquipmentType.None;
-
     public enum EquipmentType {
         None,
         MeleeWeapon,
@@ -24,41 +21,15 @@ public abstract class Equipment : MonoBehaviour, Ownable {
 
     public abstract bool isExplosive();
 
+    public abstract string getName();
+    
+    public abstract EquipmentType getType();
 
-
-
-    public virtual EquipmentType getType()
-    {
-        return equipType;
-    }
-
-    public virtual string getName()
-    {
-        return "Equipment";
-    }
-
-    public int getLevel()
-    {
-        return level;
-    }
-
-    public void setLevel(int level)
-    {
-        this.level = level;
-    }
-    public void addlevel(int level)
-    {
-        this.level += level;
-    }
-
-    public virtual bool own()
-    {
-        
-        return false;
-    }
-
+    public abstract int getLevel();
+    public abstract void setLevel(int level);
+    /*
     public virtual bool attack(int attackType)
     {
         return false;
-    }
+    }*/
 }
--- SnowStrike/Assets/Scripts/Item/Lantern.cs
+++ SnowStrike/Assets/Scripts/Item/Lantern.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Lantern : Equipment {
+
+    public int level = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +16,39 @@ public class Lantern : Equipment {
 	void Update () {
 		
 	}
+
+    public override float getRange()
+    {
+        return 1.0f * (level / 2);
+    }
+
+    public override float getDamage()
+    {
+        return 5.0f * level;
+    }
+
+    public override bool isExplosive()
+    {
+        return level > 5;
+    }
+
+    public override string getName()
+    {
+        return "item";
+    }
+
+    public override EquipmentType getType()
+    {
+        return EquipmentType.MeleeWeapon;
+    }
+
+    public override int getLevel()
+    {
+        return level;
+    }
+
+    public override void setLevel(int level)
+    {
+        this.level = level;
+    }
 }
