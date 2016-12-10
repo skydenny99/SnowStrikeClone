4cb0b8049af539d1b965064ad5fabd5c55ce437f
yangju <yangju0w0@gmail.com>
2016-12-10 21:54:20 +0900

    Item 폴더 내에 Item DB 스크립트 제작 중 허냥이에게 넘김

---
+++ SnowStrike/Assets/Scripts/Map/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject itemManager; 
+
+	// Use this for initialization
+	void Start () {
+        if(itemManager == null)
+            itemManager = GameObject.Find("Item Manager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
